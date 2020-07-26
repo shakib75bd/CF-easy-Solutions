00f1df1bbb19b49a74c5f09b12c8deb27cafcd0f
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-07-26 13:34:30 +0600

    Create CF 118A - String Task.cpp

---
+++ CF 118A - String Task.cpp
@@ -0,0 +1,34 @@
+//           ––=‹‹BISMILLAHITR RAHMANIR RAHIM››=––
+//  Problem name: CF 118A - String Task
+//  Lang: C++
+//  ©: _Shakib Hossen Shanto   _ CSE Dept__Begum Rokeya University,Rangpur
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+
+#define ll long long
+#define dl double
+#define st string
+#define ch char
+
+int main()
+{
+   unsigned n;
+   st s;
+   ll a=0; 
+
+   cin >> n;
+   while(n--)
+   {
+       cin >> s;
+       if(s=="++X" || s=="X++")  a++;
+       else if(s=="--X" || s=="X--") a--;
+   }
+
+   cout << a << endl;
+
+return 0;
+}
